9cc46836687fa491cea42926ee4a877ad92998da
DashaRyzh <trusova.d@yandex.ru>
2021-10-24 22:14:57 +0300

    Example 005 Condition if-else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Ура, это же Даша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
