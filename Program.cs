3aa3a3a2f57ab8574ebd48eeefd0e832bd07966a
DashaRyzh <trusova.d@yandex.ru>
2021-10-25 22:48:49 +0300

    It works!

--- Program.cs
+++ Program.cs
@@ -4,20 +4,22 @@ int firstFriendSpeed = 1;
 int secondFriendSpeed = 2;
 int dogSpeed = 5;
 int friend = 1;
+int time;
 
 while(distance > 10)
 {
     if(friend == 1)
     {
-        int time = distance / (firstFriendSpeed + dogSpeed);
+        time = distance / (firstFriendSpeed + dogSpeed);
         friend = 2;
     }
     else
     {
-        int time = distance / (secondFriendSpeed + dogSpeed);
+        time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
     }
-}
-distance = distance - (firstFriendSpeed + secondFriendSpeed);
+
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
 count++;
-Console.Write(count);
+}
+Console.WriteLine(count);
