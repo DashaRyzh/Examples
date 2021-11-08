1ecf304cbaae69cbb2320d7a520e1a01ef78b998
DashaRyzh <trusova.d@yandex.ru>
2021-11-08 22:48:44 +0300

    Added homework

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -3,34 +3,99 @@
 // маленькие буквы "к" заменить большими "К",
 // а большие "С" заменить маленькими "с".
 
-string text = "Крупные ИТ-компании создают настолько привлекательные условия труда, "
-            + "что уже стали законодателями мод среди работодателей. "
-            + "В ИТ совершенно нормально рассчитывать на современный офис, "
-            + "который больше похож на развлекательный центр: "
-            + "с кафе, безлимитным кофе, вкусняшками, свежими фруктами, "
-            + "тренажерным залом, штатным массажистом, "
-            + "просторными переговорными комнатами со всевозможными мягкими зонами. "
-            + "А также на помощь в релокации, доступ к корпоративной библиотеке, "
-            + "приветственные наборы и подарки на праздники — от канцелярских "
-            + "принадлежностей до термокружек, пауэрбанков и даже ноутбуков.";
+// string text = "Крупные ИТ-компании создают настолько привлекательные условия труда, "
+//             + "что уже стали законодателями мод среди работодателей. "
+//             + "В ИТ совершенно нормально рассчитывать на современный офис, "
+//             + "который больше похож на развлекательный центр: "
+//             + "с кафе, безлимитным кофе, вкусняшками, свежими фруктами, "
+//             + "тренажерным залом, штатным массажистом, "
+//             + "просторными переговорными комнатами со всевозможными мягкими зонами. "
+//             + "А также на помощь в релокации, доступ к корпоративной библиотеке, "
+//             + "приветственные наборы и подарки на праздники — от канцелярских "
+//             + "принадлежностей до термокружек, пауэрбанков и даже ноутбуков.";
 
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'С', 'с');
+
+
+// int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-
-    return result;
+    Console.WriteLine();
 }
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'С', 'с');
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }    
+    
+    int temporary = array[i];
+    array[i] = array[maxPosition];
+    array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
---
+++ Example012_for/Program.cs
@@ -0,0 +1,55 @@
+﻿// int[] arr = new int[10];
+
+// int index = 0;
+// while(index < 10)
+// {
+//     Console.Write($"{arr[index++]} ");
+
+// }
+
+
+
+
+// void DrawRectangle(int posX, int posY, int width, int height)
+// {
+//     int row = 0;
+//     while(row < height)
+//     {
+//         Console.SetCursorPosition(posX, posY + row);
+//         int count = 0;
+//         while(count < width)
+//         {
+//             Console.Write("+");
+//             count++;
+//         }
+//     row++;
+//     }
+// }
+// Console.Clear();
+// DrawRectangle(13, 5, 5, 5);
+
+// Нужно написать программу решения квадратных уравнений
+// Ax^2 + Bx + C = 0
+// 1x^2 - 2x + 6 = 0
+(double, double) Solve(double a, double b, double c)
+{
+    double d = b * b - 4 * a * c;
+    Console.WriteLine(d);
+    if (d < 0)
+    {
+        return (double.NaN, double.NaN);
+        // Console.WriteLine("Действительных корней нет.");
+    }
+    else
+    {
+        double x1 = (-b + Math.Sqrt(d))/(2*a);
+        double x2 = (-b - Math.Sqrt(d))/(2*a);
+        return(x1, x2);
+        // Console.WriteLine(x1);
+        // Console.WriteLine(x2);
+    }
+}
+Solve(1, -7, 6);
+Solve(1, 0, 6);
+Solve(1, 0, -4);
+Solve(1, 2, -4);
